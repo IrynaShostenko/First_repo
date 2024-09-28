2a12db08167dd06b8038dab7c7e62b9deea746d8
IrynaShostenko <163306036+IrynaShostenko@users.noreply.github.com>
2024-09-28 15:23:46 +0300

    fourth

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, World!")
+def factorial(n):
+    if n == 0: # базовий випадок
+        return 1
+    else:
+        return n * factorial(n-1) # рекурсивний випадок
 
-import math
+print(factorial(7)) # виведе 120
